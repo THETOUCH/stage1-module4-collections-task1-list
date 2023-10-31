f31f63601b782950976aaac5c0e8795cc068cccf
THETOUCH <super.ilgiznex@gmail.com>
2023-10-31 19:53:42 +0600

    123

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -5,5 +5,13 @@ import java.util.List;
 
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
+        ArrayList<String> newArrayList = new ArrayList<>();
+        for (int i = 1; i <= sourceList.size(); i++ ) {
+            if (i%3==0) {
+                newArrayList.add(sourceList.get(i-1));
+                newArrayList.add(sourceList.get(i-1));
+            }
+        }
+        return newArrayList;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -1,15 +1,21 @@
 package com.epam.mjc.collections.list;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        sourceList.sort(new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int c, d;
+        c = Integer.parseInt(a);
+        d = Integer.parseInt(b);
+        return 5*c*c+3 < 5*d*d+3 ? -1 : 5*c*c+3 == 5*d*d+3 ? c > d ? 1 : -1 : 1;
     }
 }
